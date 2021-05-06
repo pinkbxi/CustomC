5ac7cef07148d69f558168085a1f5be1e131c0fb
domantas prancelevicius <domantasprancelevicius@gmail.com>
2021-05-06 17:51:46 +0300

    Add project files.

---
+++ CustomC/CustomC.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace CustomC
+{
+    abstract class CustomC
+    {
+            /// <summary>
+            /// Prints Text With Your Selected Color
+            /// </summary>
+            /// <param name="Input"></param>
+            /// <param name="Color"></param>
+            public static void Print(string Input, ConsoleColor Color)
+            {
+                Console.ForegroundColor = Color;
+                Console.WriteLine(Input);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            /// <summary>
+            /// Prints Text
+            /// </summary>
+            /// <param name="Input"></param>
+            public static void Print(string Input)
+            {
+                Console.WriteLine(Input);
+            }
+            /// <summary>
+            /// Masks The Given Text And Prints It With Selected Color
+            /// </summary>
+            /// <param name="Input"></param>
+            /// <param name="Color"></param>
+            public static void Mask(string Input, ConsoleColor Color)
+            {
+                Console.ForegroundColor = Color;
+                Console.WriteLine(Input.GetHashCode());
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            /// <summary>
+            /// Masks The Given Text
+            /// </summary>
+            /// <param name="Input"></param>
+            public static void Mask(string Input)
+            {
+                Console.WriteLine(Input.GetHashCode());
+            }
+            /// <summary>
+            /// Prints Reversed String With Color
+            /// </summary>
+            /// <param name="Input"></param>
+            /// <param name="Color"></param>
+            public static void Reversed(string Input, ConsoleColor Color)
+            {
+                Char[] inp = Input.ToCharArray();
+                for (int a = inp.Length - 1; a >= 0; a--)
+                {
+                    Console.ForegroundColor = Color;
+                    Console.Write(inp[a]);
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
+            /// <summary>
+            /// Prints Reversed String
+            /// </summary>
+            /// <param name="Input"></param>
+            public static void Reversed(string Input)
+            {
+                Char[] inp = Input.ToCharArray();
+                for (int a = inp.Length - 1; a >= 0; a--)
+                {
+                    Console.Write(inp[a]);
+                }
+            }
+            /// <summary>
+            /// Does Not Close Console Until Any Button Is Pressed
+            /// </summary>
+            public static void Wait()
+            {
+                Console.Read();
+            }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CustomC Preview
+            CustomC.Mask("This Funcion Masks The Text In Hash", ConsoleColor.Green);         //Also You Can Select Any Color You Want To Be Your Texts Color
+            CustomC.Print("This Funcion Prints Text", ConsoleColor.Red);                     //Also You Can Select Any Color You Want To Be Your Texts Color
+            CustomC.Reversed("This Funcion Reverses Text Thats Given", ConsoleColor.Yellow); //Also You Can Select Any Color You Want To Be Your Texts Color
+            CustomC.Wait();                                                                  //Does Not Let Console To Close Until Button Is Pressed
+        }
+    }
+}
